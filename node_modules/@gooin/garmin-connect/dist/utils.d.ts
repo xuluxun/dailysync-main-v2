80bdac7de2cd556f51aed0602c480bc4d69de181
xuluxun <xuluxun@users.noreply.github.com>
2023-10-08 14:32:28 +0000

    Save Garmin Session

---
+++ node_modules/@gooin/garmin-connect/dist/utils.d.ts
@@ -0,0 +1,3 @@
+export declare const checkIsDirectory: (filePath: string) => boolean;
+export declare const createDirectory: (directoryPath: string) => void;
+export declare const writeToFile: (filePath: string, data: any) => void;
